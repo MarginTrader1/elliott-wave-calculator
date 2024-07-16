8db929773464161ea5e8f3752d95b85424268b0d
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-07-16 11:47:56 +0300

    немного адаптивной верстки

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -2,7 +2,7 @@ import css from "./Description.module.css";
 
 const Description = () => {
    return (
-      <div>
+      <div className={css.data}>
          <h1 className={css.title}>{"Elliott Waves Calculator"}</h1>
          <p>{"Укажите начальные значения волн в табличке"}</p>
       </div>
